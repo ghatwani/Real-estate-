5e97dfe521c4a73dad0030cef9b8e36564ade243
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-09-09 07:22:22 +0530

    createdHeader Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import SignIn from "./Pages/SignIn";
 import SignOut from "./Pages/SignOut";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
+import Header from './components/Header';
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
