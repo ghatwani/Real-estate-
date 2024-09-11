70dd99e1d3ce119747b11b030e7e8d6979f8e144
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-09-11 13:20:29 +0530

    created a middleware and custom error handle function

--- api/index.js
+++ api/index.js
@@ -28,6 +28,17 @@ app.get('/', (req, res)=>{
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
+app.use((err, req, res , next)=>{
+    const statusCode = err.statusCode || 500
+    const message= err.message || "Internal server error"
+
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
+
 app.listen(3000, () => {
     console.log("Server is running on Port 3000")
 })
