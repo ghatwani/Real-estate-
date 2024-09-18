1ea98cfd828ab5b5857419db5d11bbc6dded69d4
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-09-18 07:58:16 +0530

    complete listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from "./Pages/About";
 import Profile from "./Pages/Profile";
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './Pages/CreateListing';
 const App = () => {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ const App = () => {
 
         <Route element={<PrivateRoute/>}>
         <Route path='/profile' element={<Profile />} />
+        <Route path='/create-listing' element={<CreateListing />} />
         </Route>
 
       </Routes>
