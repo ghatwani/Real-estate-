dbc802966ac6d13a608a7a7c4f88cd81a5b4deba
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-09-25 11:12:15 +0530

    complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./Pages/Profile";
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './Pages/CreateListing';
+import UpdateListing from './Pages/UpdateListing';
 const App = () => {
   return (
     <BrowserRouter>
@@ -17,10 +18,12 @@ const App = () => {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignOut />} />
         <Route path='/about' element={<About />} />
+        {/* <Route path='/listings/:listingId' element={<Listing />} /> */}
 
         <Route element={<PrivateRoute/>}>
         <Route path='/profile' element={<Profile />} />
         <Route path='/create-listing' element={<CreateListing />} />
+        <Route path='/update-listing/:listingId' element={<UpdateListing />} />
         </Route>
 
       </Routes>
