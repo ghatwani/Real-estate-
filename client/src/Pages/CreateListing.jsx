0f2abb2f50b2a3579866697b930a269ec299e2cd
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-10-07 00:22:21 +0530

    fixing

--- client/src/Pages/CreateListing.jsx
+++ client/src/Pages/CreateListing.jsx
@@ -14,7 +14,7 @@ export default function CreateListing() {
   const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
-    imageUrls: [],
+    imageURLs: [],
     name: '',
     description: '',
     address: '',
@@ -33,7 +33,7 @@ export default function CreateListing() {
   const [loading, setLoading] = useState(false);
   console.log(formData);
   const handleImageSubmit = (e) => {
-    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+    if (files.length > 0 && files.length + formData.imageURLs.length < 7) {
       setUploading(true);
       setImageUploadError(false);
       const promises = [];
@@ -45,7 +45,7 @@ export default function CreateListing() {
         .then((urls) => {
           setFormData({
             ...formData,
-            imageUrls: formData.imageUrls.concat(urls),
+            imageURLs: formData.imageURLs.concat(urls),
           });
           setImageUploadError(false);
           setUploading(false);
@@ -90,7 +90,7 @@ export default function CreateListing() {
   const handleRemoveImage = (index) => {
     setFormData({
       ...formData,
-      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+      imageURLs: formData.imageURLs.filter((_, i) => i !== index),
     });
   };
 
@@ -128,7 +128,7 @@ export default function CreateListing() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      if (formData.imageUrls.length < 1)
+      if (formData.imageURLs.length < 1)
         return setError('You must upload at least one image');
       if (+formData.regularPrice < +formData.discountedPrice)
         return setError('Discount price must be lower than regular price');
@@ -339,8 +339,8 @@ export default function CreateListing() {
           <p className='text-red-700 text-sm'>
             {imageUploadError && imageUploadError}
           </p>
-          {formData.imageUrls.length > 0 &&
-            formData.imageUrls.map((url, index) => (
+          {formData.imageURLs.length > 0 &&
+            formData.imageURLs.map((url, index) => (
               <div
                 key={url}
                 className='flex justify-between p-3 border items-center'
