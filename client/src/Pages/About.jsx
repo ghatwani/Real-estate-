81d834eb95042d30d692b2a22ccb7494da52131f
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-10-06 20:44:38 +0530

    complete about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const About = () => {
   return (
     <div className="py-20 px-4 mx-auto max-w-6xl">
-      <h1 className="text-3xl font-bold mb-4 text-slate-800 ">About Ghar Dekho</h1>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 ">
+        About Ghar Dekho
+      </h1>
       <p className="mb-4 text-slate-700 ">
-        Sahand Estate is a leading real estate agency that specializes in
-        helping clients buy, sell, and rent properties in the most desirable
+        Ghar Dekho is a leading real estate agency that specializes in helping
+        clients buy, sell, and rent properties in the most desirable
         neighborhoods. Our team of experienced agents is dedicated to providing
         exceptional service and making the buying and selling process as smooth
         as possible.
